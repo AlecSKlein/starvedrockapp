1bc9c3f2cbbd0f25381baa0e02cd003284023b13
Kipros Moustoukkis <kiprosmoustoukkis@Kipros-Moustoukkiss-MacBook-Pro.local>
2014-10-27 18:04:31 -0500

    The slider functions should be fully implemented. The side views don't show anything yet, but the slider works.

---
+++ StarvedRockApp/NewsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsViewController.swift
+//  StarvedRockApp
+//
+//  Created by Kipros Moustoukkis on 10/20/14.
+//  Copyright (c) 2014 Kipros Moustoukkis. All rights reserved.
+//
+
+import UIKIT
+
+class NewsViewController: UIViewController {
+    
+}
