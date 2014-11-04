ad8b6c5f651b1ae46c9a4f37c08d4d178745c5f8
josh <bates.a.joshua@gmail.com>
2014-11-03 19:07:44 -0600

    updates

--- StarvedRock/gen/com/example/starvedrock/R.java
+++ StarvedRock/gen/com/example/starvedrock/R.java
@@ -233,6 +233,7 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int poi_view=0x7f030001;
     }
     public static final class string {
         public static final int action_settings=0x7f06001c;
